b81baaee1b3e6971433548ea9ed9051546d80ea3
sasuke19y <shubhranil968@gmail.com>
2023-10-14 13:17:05 +0530

    Create to calculate the area of regular hexagon.c

---
+++ to calculate the area of regular hexagon.c
@@ -0,0 +1,10 @@
+//
+// Created by Lenovo on 14/10/2023.
+#include<stdio.h>
+int main(){
+    float area, a;
+    printf("Enter the value of a: \n");
+    scanf("%f", &a);
+    area = 3 * 1.732 * 0.5 * a * a;
+    printf("Area of the hexagon;%f", area);
+}
